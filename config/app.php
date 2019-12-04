6e376c764eb9981914c382f64db03c55ed5f0124
QuyetNguyen95 <54739406+QuyetNguyen95@users.noreply.github.com>
2019-12-04 16:32:13 +0700

    fix loi ko vao duoc admin

--- config/app.php
+++ config/app.php
@@ -159,6 +159,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        \Modules\Admin\Providers\RouteServiceProvider::class,
+        \Modules\Admin\Providers\AdminServiceProvider::class
        
 
     ],
@@ -209,7 +211,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        // 'Debugbar' => Barryvdh\Debugbar\Facade::class,
+         'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
